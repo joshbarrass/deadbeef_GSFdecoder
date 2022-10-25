4a33a5528637feeb58fc1d960ca4ff76f7fb2a25
Joshua Barrass <josh.barrass.work@gmail.com>
2022-10-25 12:32:13 +0100

    Make sample rate configurable at compile-time

--- include/consts.h
+++ include/consts.h
@@ -1,6 +1,9 @@
 #ifndef GSF_CONSTS_H
 #define GSF_CONSTS_H 1
 
-constexpr int SAMPLE_RATE = 44100;
+#ifndef PLAYBACK_RATE
+#define PLAYBACK_RATE 44100
+#endif
+constexpr int SAMPLE_RATE = PLAYBACK_RATE;
 
 #endif
