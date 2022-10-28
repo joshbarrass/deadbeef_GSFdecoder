9ce86e19d8cecb1bb972c3577afe906e68c948b9
Joshua Barrass <josh.barrass.work@gmail.com>
2022-10-28 12:16:21 +0100

    Slightly more intuitive def

--- include/consts.h
+++ include/consts.h
@@ -1,8 +1,8 @@
 #ifndef GSF_CONSTS_H
 #define GSF_CONSTS_H 1
 
-#ifndef PLAYBACK_RATE
-#define PLAYBACK_RATE 44100
+#ifndef SAMPLERATE
+#define SAMPLERATE 44100
 #endif
 constexpr int SAMPLE_RATE = PLAYBACK_RATE;
 
